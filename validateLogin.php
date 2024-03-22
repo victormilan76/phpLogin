88b07ff3e1b87d57d6ce797d54221e20c7363d8c
Victor Milan Paucar Pinto <victormilan76@gmail.com>
2024-03-22 09:48:42 -0400

    Update validateLogin.php
    cambios en el modelo de negocio para consultar Login

--- validateLogin.php
+++ validateLogin.php
@@ -1,19 +1,17 @@
 <?php
-    //llamamos a la cadena de conexion
+    //cadena de conexion
     include("conection.php");
     $con=conectarse();
    
-   $user = $_POST['user'];
-	$password = $_POST['password'];
+    $user = $_POST['user'];
+    $password = $_POST['password'];
    
-   //echo $user."-".$password;
-   
-	$query = "SELECT ID_USER,NAME,USER,PASSWORD,DATE_CREATION,ROL,STATUS FROM USER where USER='$user' and PASSWORD='$password'";
+	$query = "SELECT US_ID_BI,US_NAME_VC,US_USER_VC,US_PASSWORD_VC,US_DATE_CREATION_DT,US_ROL_BT,US_STATUS_BT FROM USER where US_USER_VC='$user' and US_PASSWORD_VC='$password'";
 	$result = mysqli_query($con,$query);
 	if(!$result){die("query Failed"); }
 	$row = mysqli_fetch_array($result);
 
-   if ($row['USER']==$user && $row['PASSWORD']==$password)
+   if ($row['US_USER_VC']==$user && $row['US_PASSWORD_VC']==$password)
 	{
       echo "<script> window.location.href='home.html';</script>";
    }
