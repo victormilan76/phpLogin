f7fdfe5229f02ad9fd21f91c654408667fb3b297
MILAN <victormilan76@gmail.com>
2024-03-24 14:02:24 -0600

    cambios de interfaz formulario responsivo

--- validateLogin.php
+++ validateLogin.php
@@ -1,22 +1,19 @@
 <?php
-    //cadena de conexion
-    include("conection.php");
-    $con=conectarse();
+   //cadena de conexion
+   include("conection.php");
+   $con=conectarse();
    
-    $user = $_POST['user'];
-    $password = $_POST['password'];
-   
+   $user = $_POST['user'];
+   $password = $_POST['password'];
+
 	$query = "SELECT US_ID_BI,US_NAME_VC,US_USER_VC,US_PASSWORD_VC,US_DATE_CREATION_DT,US_ROL_BT,US_STATUS_BT FROM USER where US_USER_VC='$user' and US_PASSWORD_VC='$password'";
 	$result = mysqli_query($con,$query);
 	if(!$result){die("query Failed"); }
 	$row = mysqli_fetch_array($result);
 
    if ($row['US_USER_VC']==$user && $row['US_PASSWORD_VC']==$password)
-	{
       echo "<script> window.location.href='home.html';</script>";
-   }
    else
-   {
       echo "<script> window.location.href='index.html';</script>";
-   }
+   
 ?>
